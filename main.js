c1c1514f1020d71c956faa620485a3837227f4e0
Dezeli <haterecursive@gmail.com>
2023-09-05 12:59:40 +0900

    Update

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 const bg = document.getElementById("bg");
 const mainBtns = document.getElementById("mainMenu");
 const returnBtn = document.getElementById("return");
-const howCont = document.getElementById("how");
 const racs = document.getElementById("racs");
-const chooseBtns = document.getElementById("choose");
 const racA = document.getElementById("racA");
 const racB = document.getElementById("racB");
 const racC = document.getElementById("racC");
+const chooseBtns = document.getElementById("choose");
+const stage = document.getElementById("stage")
+
+
+var score = 0;
+
+function showStage(stagenum){
+    bg.style.backgroundImage = "url(Images/stage_bg.png)";
+    mainBtns.style.display = "none";
+    stage.style.display = "flex";
+
+    var stageTag = stage.getElementsByTagName("h3")[0];
+    var scoreTag = stage.getElementsByTagName("p")[0];
+
+    stageTag.innerText = "Stage " + stagenum;
+    scoreTag.innerText = "Score : " + score;
+    stagenum += 1;
+
+    setTimeout(showGame, 3000);
+}
 
 function showGame(){
     bg.style.backgroundImage = "url(Images/game_bg.png)";
-    mainBtns.style.display = "none";
     returnBtn.style.display = "flex";
     racs.style.display = "flex";
     chooseBtns.style.display = "flex";
+    stage.style.display = "none";
 
     var timer = 0;
     function frame(){
@@ -38,14 +56,15 @@ function showGame(){
 function showHow(){
     bg.style.backgroundImage = "url(Images/intro_bg.png)";
     mainBtns.style.display = "none";
-    howCont.style.display = "block";
     returnBtn.style.display = "flex";
+    returnBtn.style.alignContent = "flex-end";
 }
 
 function showRank(){
     bg.style.backgroundImage = "url(Images/rank_bg.png)";
     mainBtns.style.display = "none";
     returnBtn.style.display = "flex";
+    returnBtn.style.alignContent = "flex-start";
 }
 
 
@@ -53,7 +72,6 @@ function showMenu(){
     bg.style.backgroundImage = "url(Images/menu_bg.png)";
     mainBtns.style.display = "flex";
     returnBtn.style.display = "none";
-    howCont.style.display = "none";
     racs.style.display = "none";
     chooseBtns.style.display = "none";
     cancelAnimationFrame(racmove);
