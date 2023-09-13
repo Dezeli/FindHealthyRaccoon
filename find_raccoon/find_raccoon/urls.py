2d99fbde98330805bb1f45d68f145873083011b8
Dezeli <haterecursive@gmail.com>
2023-09-13 12:43:16 +0900

    Update

--- find_raccoon/find_raccoon/urls.py
+++ find_raccoon/find_raccoon/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Raccoon project.
+URL configuration for find_raccoon project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include("Game.urls")),
+    path("", include("game.urls")),
     path('admin/', admin.site.urls),
 ]
